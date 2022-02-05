0df5abe3397498619ffb15e4a5cbc6ee664dedf0
Nachcha Aroonrerk (Neen) <neen.nachcha@gmail.com>
2022-02-06 06:03:28 +0700

    Add prefilter function to server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,42 @@
+const API_KEY = require('../config.js').API_KEY;
 const express = require('express');
+
 let app = express();
 let port = 8080;
 
+const { JSDOM } = require( "jsdom" );
+const { window } = new JSDOM( "" );
+const $ = require( "jquery" )( window );
+
+
 // middleware
 app.use(express.static('client/dist'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.end();
 });
+
+app.all('/*', (req, res) => {
+  var url = req.url;
+  var method = req.method;
+  var data = req.body;
+
+  $.ajaxPrefilter(function (options, originalOptions, jqXHR) {
+    jqXHR.setRequestHeader('Authorization', API_KEY);
+  })
+
+  $.ajax({
+    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp${url}`,
+    method: method,
+    data: data,
+    success: function (results) {
+      res.send(results);
+    }
+  });
+})
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
