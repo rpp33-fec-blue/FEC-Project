234af9622aa90f6bbfcfc10c79e0b60c16e70b3c
Joseph Casey <josephcasey@pop-os.localdomain>
2022-02-06 20:02:33 -0500

    Somehow lost addOutfit when I rebased. Adding it back in

---
+++ client/src/action-creators/addOutfit.js
@@ -0,0 +1,18 @@
+import actionOutfit from '../actions/outfit.js';
+import store from '../configureStore.js';
+
+var addOutfit = ( productId ) => {
+
+  return ( dispatch ) => {
+
+    var outfits = store.getState().outfits;
+    var index = outfits.indexOf( productId );
+    if ( index === -1 ) {
+      outfits.push( productId );
+    }
+
+    dispatch( actionOutfit( outfits ));
+  };
+};
+
+export default addOutfit;
