ce4f3562385d1e3361a8f0661d0397b60e6038ad
J.P. Doyle <jp.hackreactor@gmail.com>
2022-02-06 18:29:02 -0800

    Refactored Redux reducers

---
+++ client/src/reducers/changeOutfit.js
@@ -0,0 +1,10 @@
+import initialState from './initialState';
+
+const changeOutfit = (state = initialState.outfit, action) => {
+  if (action.type === 'CHANGE_OUTFIT') {
+    return action.outfit;
+  }
+  return state;
+};
+
+export default changeOutfit;
