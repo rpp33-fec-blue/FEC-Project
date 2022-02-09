50d732fa9b9f7472a9069131880de40994efe492
Joseph Casey <josephcasey@pop-os.localdomain>
2022-02-09 08:17:25 -0500

    Added reducer tests

--- test/redux/test.js
+++ test/redux/test.js
@@ -1,3 +1,4 @@
+// Actions
 import changeCart from '../../client/src/actions/cart.js';
 import changeOutfit from '../../client/src/actions/outfit.js';
 import changeProductId from '../../client/src/actions/productId.js';
@@ -7,6 +8,17 @@ import changeQuestions from '../../client/src/actions/questions.js';
 import changeMetadata from '../../client/src/actions/metadata.js';
 import changeStyles from '../../client/src/actions/styles.js';
 import changeProductInfo from '../../client/src/actions/productInfo.js';
+
+// Reducers
+import reducerCart from '../../client/src/reducers/changeCart.js';
+import reducerOutfit from '../../client/src/reducers/changeOutfit.js';
+import reducerProductId from '../../client/src/reducers/changeProduct.js';
+import reducerRelatedItems from '../../client/src/reducers/changeRelated.js';
+import reducerReviews from '../../client/src/reducers/changeReviews.js';
+import reducerQuestions from '../../client/src/reducers/changeQuestions.js';
+import reducerMetadata from '../../client/src/reducers/changeMetadata.js';
+import reducerStyles from '../../client/src/reducers/changeStyles.js';
+import reducerProductInfo from '../../client/src/reducers/changeProductInfo.js';
 
 describe( 'Actions', () => {
   describe( 'Product Id', () => {
@@ -135,4 +147,83 @@ describe( 'Actions', () => {
       expect( changeProductInfo( 'test' ).productInfo ).toEqual( 'test' );
     });
   });
-})
+});
+
+describe( 'Reducers', () => {
+  var fakeAction = ( value ) => ({
+    type: 'FAKE',
+    value: value
+  });
+  describe( 'Cart', function() {
+    it( 'should change state when changeCart action is passed in', function() {
+      expect( reducerCart( 'old state', changeCart( 'new state' ) ) ).toEqual( 'new state' );
+    });
+    it( 'should retain original state when a different action is passed in', function() {
+      expect( reducerCart( 'old state', fakeAction( 'new state' ) ) ).toEqual( 'old state' );
+    });
+  });
+  describe( 'Outfit', function() {
+    it( 'should change state when changeOutfit action is passed in', function() {
+      expect( reducerOutfit( 'old state', changeOutfit( 'new state' ) ) ).toEqual( 'new state' );
+    });
+    it( 'should retain original state when a different action is passed in', function() {
+      expect( reducerOutfit( 'old state', fakeAction( 'new state' ) ) ).toEqual( 'old state' );
+    });
+  });
+  describe( 'Product Id', function() {
+    it( 'should change state when changeProductId action is passed in', function() {
+      expect( reducerProductId( 'old state', changeProductId( 'new state' ) ) ).toEqual( 'new state' );
+    });
+    it( 'should retain original state when a different action is passed in', function() {
+      expect( reducerProductId( 'old state', fakeAction( 'new state' ) ) ).toEqual( 'old state' );
+    });
+  });
+  describe( 'Related Items', function() {
+    it( 'should change state when changeRelatedItems action is passed in', function() {
+      expect( reducerRelatedItems( 'old state', changeRelatedItems( 'new state' ) ) ).toEqual( 'new state' );
+    });
+    it( 'should retain original state when a different action is passed in', function() {
+      expect( reducerRelatedItems( 'old state', fakeAction( 'new state' ) ) ).toEqual( 'old state' );
+    });
+  });
+  describe( 'Reviews', function() {
+    it( 'should change state when changeReviews action is passed in', function() {
+      expect( reducerReviews( 'old state', changeReviews( 'new state' ) ) ).toEqual( 'new state' );
+    });
+    it( 'should retain original state when a different action is passed in', function() {
+      expect( reducerReviews( 'old state', fakeAction( 'new state' ) ) ).toEqual( 'old state' );
+    });
+  });
+  describe( 'Questions', function() {
+    it( 'should change state when changeQuestions action is passed in', function() {
+      expect( reducerQuestions( 'old state', changeQuestions( 'new state' ) ) ).toEqual( 'new state' );
+    });
+    it( 'should retain original state when a different action is passed in', function() {
+      expect( reducerQuestions( 'old state', fakeAction( 'new state' ) ) ).toEqual( 'old state' );
+    });
+  });
+  describe( 'Metadata', function() {
+    it( 'should change state when changeMetadata action is passed in', function() {
+      expect( reducerMetadata( 'old state', changeMetadata( 'new state' ) ) ).toEqual( 'new state' );
+    });
+    it( 'should retain original state when a different action is passed in', function() {
+      expect( reducerMetadata( 'old state', fakeAction( 'new state' ) ) ).toEqual( 'old state' );
+    });
+  });
+  describe( 'Styles', function() {
+    it( 'should change state when changeStyles action is passed in', function() {
+      expect( reducerStyles( 'old state', changeStyles( 'new state' ) ) ).toEqual( 'new state' );
+    });
+    it( 'should retain original state when a different action is passed in', function() {
+      expect( reducerStyles( 'old state', fakeAction( 'new state' ) ) ).toEqual( 'old state' );
+    });
+  });
+  describe( 'Product Info', function() {
+    it( 'should change state when changeProductInfo action is passed in', function() {
+      expect( reducerProductInfo( 'old state', changeProductInfo( 'new state' ) ) ).toEqual( 'new state' );
+    });
+    it( 'should retain original state when a different action is passed in', function() {
+      expect( reducerProductInfo( 'old state', fakeAction( 'new state' ) ) ).toEqual( 'old state' );
+    });
+  });
+});
