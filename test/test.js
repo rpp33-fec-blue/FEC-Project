8cd17008eca42d1256add8ee5e81fc9d08f51227
Joseph Casey <josephcasey@pop-os.localdomain>
2022-02-10 21:17:57 -0500

    Fixed GET API tests

--- test/test.js
+++ test/test.js
@@ -46,38 +46,33 @@ describe( 'API', () => {
       });
   });
   it( 'should be able to get data from /products/:${productId}/related', () => {
-    return axios.get( 'http://localhost:8080/products/:64620/related', { params: { product_id: 64620 } } )
+    return axios.get( 'http://localhost:8080/products/64620/related', { params: { product_id: 64620 } } )
       .then( ( results ) => {
-        console.log('related:', results.data);
-        expect(results.data.data.results.length).toBeGreaterThan(0);
+        expect(results.data.data.length).toBeGreaterThan(0);
       });
   });
   it( 'should be able to get data from /reviews', () => {
     return axios.get( 'http://localhost:8080/reviews', { params: { product_id: 64620, count: 1000 } } )
       .then( ( results ) => {
-        console.log('reviews:', results.data);
         expect(results.data.data.results.length).toBeGreaterThan(0);
       });
   });
   it( 'should be able to get data from /reviews/meta', () => {
     return axios.get( 'http://localhost:8080/reviews/meta', { params: { product_id: 64620 } } )
       .then( ( results ) => {
-        console.log('metadata:', results.data);
-        expect(results.data.data.results.length).toBeGreaterThan(0);
+        expect(results.data.data.product_id).toEqual('64620');
       });
   });
   it( 'should be able to get data from /products/:${productId}/styles', () => {
-    return axios.get( `http://localhost:8080/products/:64620/styles`, { params: { product_id: productId } } )
+    return axios.get( `http://localhost:8080/products/64620/styles`, { params: { product_id: 64620 } } )
       .then( ( results ) => {
-        console.log('styles:', results.data);
-        expect(results.data.data.results.length).toBeGreaterThan(0);
+        expect(results.data.data.product_id).toEqual('64620');
       });
   });
   it( 'should be able to get data from /products/:${productId}', () => {
-    return axios.get( `http://localhost:8080/products/:64620`, { params: { product_id: 64620 } } )
+    return axios.get( `http://localhost:8080/products/64620`, { params: { product_id: 64620 } } )
       .then( ( results ) => {
-        console.log('product info:', results.data);
-        expect(results.data.data.results.length).toBeGreaterThan(0);
+        expect(results.data.data.id).toEqual(64620);
       });
   });
 });
