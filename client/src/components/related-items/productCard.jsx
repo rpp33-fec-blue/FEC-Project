a81f8c0118bcc54df3d54f4f5330f6cf77a50d2c
Joseph Casey <josephcasey@pop-os.localdomain>
2022-02-13 15:30:26 -0500

    changes to productCard and productList

--- client/src/components/related-items/productCard.jsx
+++ client/src/components/related-items/productCard.jsx
@@ -7,8 +7,12 @@ class ProductCard extends React.Component {
 
   render() {
     return (
-      <div>
-        <div>ITEM</div>
+      <div className='product-card'>
+        <img className='card-photo' src={this.props.item.photo}></img>
+        <div className='card-category'>{this.props.item.category}</div>
+        <div className='card-product-name'>{this.props.item.name}</div>
+        <div className='card-price'>{this.props.item.price}</div>
+        <div className='card-rating'>{this.props.item.rating}</div>
       </div>
     );
   }
--- client/src/components/related-items/productList.jsx
+++ client/src/components/related-items/productList.jsx
@@ -4,12 +4,21 @@ import ProductCard from './productCard.jsx';
 class ProductList extends React.Component {
   constructor(props) {
     super(props);
+    this.items = this.buildRelatedItemsData();
+    console.log('related:', this.props.relatedItems);
   }
+
+  buildRelatedItemsData() {
+    // call GET on the api for every item in the relatedItems array
+    // build up an array of items that has all of the info from get requests
+    return this.props.relatedItems;
+  }
+
 
   render() {
     return (
       <div>
-        {this.props.relatedItems.map( ( item ) => {
+        {this.items.map( ( item ) => {
           return (
             <ProductCard item={ item }/>
           )
