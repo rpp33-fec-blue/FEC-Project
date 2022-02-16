70517d30a5f2dcdbfb589ae90d4a262d36a35c44
Joseph Casey <josephcasey@pop-os.localdomain>
2022-02-15 22:15:44 -0500

    Getting feature ready for merge

--- client/src/components/related-items/productCard.jsx
+++ client/src/components/related-items/productCard.jsx
@@ -3,14 +3,12 @@ import React from 'react';
 class ProductCard extends React.Component {
   constructor(props) {
     super(props);
-    console.log('productCard props:', this.props);
   }
 
   render() {
     var photo = '';
     if ( this.props.item.styles ) {
       photo = this.props.item.styles.photos[0].url
-      console.log(photo);
       if ( photo === null ) {
         photo = './assets/light-grey.jpg';
       }
