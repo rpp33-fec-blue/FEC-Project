3bb428e06855e1e081cdef8b908a02bad828bff5
J.P. Doyle <jp.hackreactor@gmail.com>
2022-02-15 19:50:50 -0800

    Refactored <OutfitToggle />

--- client/src/components/overview/OutfitToggle.jsx
+++ client/src/components/overview/OutfitToggle.jsx
@@ -1,13 +1,26 @@
 const OutfitToggle = ( { productId, outfit, addOutfit, removeOutfit } ) => {
 
-  // If in outfit
-  //   Return full star that can remove outfit
-  // Else
-  //   Return empty star that can add outfit
+  var inOutfit = false;
+  for (var index = 0; index < outfit.length; index++) {
+    if (outfit[index] === productId) {
+      inOutfit = true;
+      break;
+    }
+  }
 
-  return (
-    <div></div>
-  );
+  if (inOutfit) {
+    return (
+      <div>
+        <span onClick={removeOutfit}>Remove from outfit</span> {/* TO DO - return full star that can remove outfit */}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <span onClick={addOutfit}>Add to outfit</span> {/* TO DO - remove empty star that can add outfit */}
+      </div>
+    );
+  }
 };
 
 export default OutfitToggle;
