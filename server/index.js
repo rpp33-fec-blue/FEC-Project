d57fda4c55175da632ec3a3bf8aaf679077d8cca
Nachcha Aroonrerk (Neen) <neen.nachcha@gmail.com>
2022-02-18 11:13:06 +0700

    Try .env file

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
-const API_KEY = require('../config.js').API_KEY;
+// const API_KEY = require('../config.js').API_KEY;
+const API_KEY = process.env.API_KEY;
 const express = require('express');
 const axios = require( "axios" );
 
