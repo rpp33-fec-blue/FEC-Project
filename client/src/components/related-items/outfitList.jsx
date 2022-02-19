312b574403b62e733deb713bf16aa6675f034b58
Joseph Casey <josephcasey@pop-os.localdomain>
2022-02-19 11:44:35 -0500

    add outfit feature

--- client/src/components/related-items/outfitList.jsx
+++ client/src/components/related-items/outfitList.jsx
@@ -1,15 +1,62 @@
 import React from 'react';
 import ProductCard from './productCard.jsx';
+import AddOutfitCard from './addOutfitCard.jsx';
 
 class OutfitList extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      items: []
+    }
+    this.isReady = false;
+  }
+
+  buildOutfitList() {
+    var apiCalls = [];
+    for ( var i = 0; i < this.props.outfit.length; i++ ) {
+      var productId = this.props.outfit[i];
+      apiCalls.push(axios.get( `http://localhost:8080/products/${productId}`, { params: { product_id: productId } } ));
+      apiCalls.push(axios.get( 'http://localhost:8080/reviews/meta', { params: { product_id: productId } } ));
+      apiCalls.push(axios.get( `http://localhost:8080/products/${productId}/styles`, { params: { product_id: productId } } ));
+    }
+
+    Promise.all(apiCalls).then( ( results ) => {
+      var outfitArray = [];
+      for ( var call = 0; call < results.length; call+=3 ) {
+        var product = Object.assign( results[ call ].data.data, results[ call + 1 ].data.data );
+        for ( var style = 0; style < results[ call + 2 ].data.data.results.length; style++ ) {
+          if ( results[ call + 2 ].data.data.results[ style ]['default?'] ) {
+            product = Object.assign( product, { styles: results[ call + 2 ].data.data.results[ style ] } );
+            break;
+          }
+        }
+        outfitArray.push( product );
+      }
+
+      this.isReady = true;
+      console.log('outfitArray:', outfitArray);
+      this.setState({
+        items: outfitArray
+      });
+    });
+  }
+
+  addToOutfit() {
+    this.isReady = false;
+    this.props.addToOutfit();
   }
 
   render() {
+
+    if ( !this.isReady ) {
+      this.buildOutfitList();
+      return null;
+    }
+
     return (
-      <div>
-        {this.props.outfit.map( ( item ) => {
+      <div className='card-list'>
+        <AddOutfitCard addToOutfit={this.addToOutfit.bind( this )}/>
+        {this.state.items.map( ( item ) => {
           return (
             <ProductCard item={ item } changeProduct={this.props.changeProduct}/>
           );
--- client/src/components/related-items/relatedItems.jsx
+++ client/src/components/related-items/relatedItems.jsx
@@ -11,13 +11,17 @@ class RelatedItems extends React.Component {
     this.props.handleSwitchProduct( productId );
   }
 
+  addToOutfit() {
+    this.props.handleAddOutfit( this.props.productId );
+  }
+
   render() {
     return (
       <div className='item-widget-related-item'>
         <div>RELATED PRODUCTS</div>
         <ProductList relatedProducts={this.props.relatedProducts} changeProduct={this.changeProduct.bind( this )}/>
         <div>YOUR OUTFIT</div>
-        <OutfitList outfit={this.props.outfit} changeProduct={this.changeProduct.bind( this )}/>
+        <OutfitList outfit={this.props.outfit} changeProduct={this.changeProduct.bind( this )} addToOutfit={this.addToOutfit.bind( this)}/>
       </div>
     );
   }
