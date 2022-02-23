eb671226a53276ace6fb57b716af6fa309962165
Joseph Casey <josephcasey@pop-os.localdomain>
2022-02-22 22:14:11 -0500

    Added in clustering on server

--- server/index.js
+++ server/index.js
@@ -2,44 +2,59 @@ const API_KEY = require('../config.js').API_KEY;
 // const API_KEY = process.env.API_KEY;
 const express = require('express');
 const axios = require( "axios" );
+const cluster = require('cluster');
+const numberOfCores = require('os').cpus().length;
 
 let app = express();
 let port = 8080;
 
-// middleware
-app.use(express.static('client/dist'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+if ( cluster.isMaster ) {
+  console.log(`Number of CPU cores: ${numberOfCores}`);
 
-app.get('/', (req, res) => {
-  res.end();
-});
+  for ( let i = 0; i < numberOfCores; i++ ) {
+    cluster.fork();
+  }
 
-app.all('/*', (req, res) => {
-  var url = req.url;
-  var method = req.method;
-  var data = req.body;
-  console.log('data:', data);
-  console.log('url:', url);
+  cluster.on( 'exit', ( worker, code, signal ) => {
+    console.log(`Worker ${worker.process.pid} went offline`);
+    console.log('Creating another worker');
+    cluster.fork();
+  });
+} else {
 
-  axios({
-    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp${url}`,
-    headers: { "Authorization": API_KEY },
-    method: method,
-    data: data
-  })
-  .then( ( results ) => {
-    res.send({ data: results.data } );
-  })
-  .catch( ( error ) => {
-    console.log( 'error:', error );
+  app.use(express.static('client/dist'));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  app.get('/', (req, res) => {
     res.end();
+  });
+
+  app.all('/*', (req, res) => {
+    var url = req.url;
+    var method = req.method;
+    var data = req.body;
+    console.log('url:', url);
+
+    axios({
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp${url}`,
+      headers: { "Authorization": API_KEY },
+      method: method,
+      data: data
+    })
+    .then( ( results ) => {
+      res.send({ data: results.data } );
+    })
+    .catch( ( error ) => {
+      console.log( 'error:', error );
+      res.end();
+    })
   })
-})
 
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+}
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
 
 module.exports
