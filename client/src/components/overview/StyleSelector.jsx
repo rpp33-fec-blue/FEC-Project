955332dc8f4ae8c4fb77f80bf02566247edfdf98
J.P. Doyle <jp.hackreactor@gmail.com>
2022-02-22 19:28:04 -0800

    Fixed <StyleSelector /> bug

--- client/src/components/overview/StyleSelector.jsx
+++ client/src/components/overview/StyleSelector.jsx
@@ -13,8 +13,8 @@ const StyleSelector = ( { styles, selectedStyleIndex, updateSelectedStyle } ) =>
     <div className='styleSelector-component'>
       <p>Selected style: {selectedStyleName}</p>
       {styles.results.map((style, index) =>  index === selectedStyleIndex ?
-        <img src={style.photos[index].thumbnail_url} className='style-thumbnail' id='selectedStyle' key={index}></img> :
-        <img src={style.photos[index].thumbnail_url} className='style-thumbnail' onClick={updateSelectedStyle} index={index} key={index}></img>
+        <img src={style.photos[0].thumbnail_url} className='style-thumbnail' id='selectedStyle' key={index}></img> :
+        <img src={style.photos[0].thumbnail_url} className='style-thumbnail' onClick={updateSelectedStyle} index={index} key={index}></img>
       )}
    </div>
   );
