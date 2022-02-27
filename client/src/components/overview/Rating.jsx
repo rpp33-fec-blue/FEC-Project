408fcf510237bed977b5e540b16f13a520ed7a28
J.P. Doyle <jp.hackreactor@gmail.com>
2022-02-27 11:21:40 -0800

    Updated <OutfitToggle /> and <Rating />

--- client/src/components/overview/Rating.jsx
+++ client/src/components/overview/Rating.jsx
@@ -1,42 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import statePropTypes from '../prop-types.js';
+import ReviewStars from '../reviewStars.jsx'
 import ErrorBoundary from '../ErrorBoundary.jsx';
 
 const Rating = ({ metadata }) => {
 
+  var noReviews = false;
   var numberOfRatings = 0;
   var sumOfRatings = 0;
-  for (var rating in metadata.ratings) {
-    var numberOfRating = Number(metadata.ratings[rating]);
-    numberOfRatings += numberOfRating;
-    sumOfRatings += (numberOfRating * rating);
-  }
-  const averageRating = sumOfRatings / numberOfRatings;
-  const wholeStars = Math.floor(averageRating);
-  const partialStar = averageRating - wholeStars;
-  var roundedAverageRating;
-  if (partialStar !== 0) {
-    if (partialStar > 0 && partialStar < 0.125) {
-      roundedAverageRating = wholeStars;
-    } else if (partialStar >= 0.125 && partialStar < 0.375) {
-      roundedAverageRating = wholeStars + 0.250;
-    } else if (partialStar >= 0.375 && partialStar < 0.625) {
-      roundedAverageRating = wholeStars + 0.500;
-    } else if (partialStar >= 0.625 && partialStar < 0.875) {
-      roundedAverageRating = wholeStars + 0.750;
-    } else {
-      roundedAverageRating = wholeStars + 1;
+
+  if (Object.keys(metadata.ratings).length === 0) {
+    noReviews = true;
+  } else {
+    for (var rating in metadata.ratings) {
+      var numberOfRating = Number(metadata.ratings[rating]);
+      numberOfRatings += numberOfRating;
+      sumOfRatings += (numberOfRating * rating);
     }
   }
 
-  return (
-    <ErrorBoundary component={'Rating'}>
-      <div className='rating-component'>
-        <span>Rating: {roundedAverageRating} stars</span><span href='#'> Read all {numberOfRatings} reviews</span> {/* TO DO - convert to actual stars and link to reviews */}
-      </div>
-    </ErrorBoundary>
-  );
+  if (noReviews) {
+    return null;
+  } else {
+    return (
+      <ErrorBoundary component={'Rating'}>
+        <div className='rating-component'>
+          <ReviewStars ratings={metadata.ratings} /> <a href='#'>Read all {numberOfRatings} reviews</a>
+        </div>
+      </ErrorBoundary>
+    );
+  }
 };
 
 Rating.propTypes = {
