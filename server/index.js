9b00afad355e4354fa377c831c755bdb77b06892
Joseph Casey <joecasey22@gmail.com>
2022-03-05 08:30:27 -0500

    Added dotenv

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
-const API_KEY = require('../config.js').API_KEY;
+require('dotenv').config()
+const API_KEY = process.env.API_KEY
 const express = require('express');
 const axios = require( "axios" );
 const cluster = require('cluster');
