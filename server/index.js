956321e9ead50776e73f3ab31f1a4fc65ee6f1ff
Joseph Casey <josephcasey@pop-os.localdomain>
2022-03-12 08:57:12 -0500

    Back to gzip...

--- server/index.js
+++ server/index.js
@@ -40,8 +40,8 @@ var applyMiddleware = () => {
 
 var sendCompressedBundle = () => {
   app.get('*.js', (req, res, next) => {
-    req.url = req.url + '.br';
-    res.set('Content-Encoding', 'br');
+    req.url = req.url + '.gz';
+    res.set('Content-Encoding', 'gzip');
     res.set('Content-Type', 'application/javascript; charset=UTF-8');
     next();
   });
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 var path = require('path');
 const CompressionWebpackPlugin = require("compression-webpack-plugin");
 const TerserPlugin = require('terser-webpack-plugin');
-const zlib = require("zlib");
 
 
 module.exports = {
@@ -36,17 +35,9 @@ module.exports = {
   },
   plugins: [
     new CompressionWebpackPlugin({
-      filename: "[path][base].br",
-      algorithm: "brotliCompress",
-      test: /\.(js|css|html|svg)$/,
-      compressionOptions: {
-        params: {
-          [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
-        },
-      },
-      threshold: 10240,
-      minRatio: 0.8,
-      deleteOriginalAssets: false,
-    }),
-  ],
+      filename: "[file].gz",
+      algorithm: "gzip",
+      test: /\.js$/,
+    })
+  ]
 }
